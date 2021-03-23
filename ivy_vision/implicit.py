0c68d1a866b5e5b879ecb580b4d310b5de0a8a5d
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-23 09:03:42 +0100

    added more implicit rendering functions.

--- ivy_vision/implicit.py
+++ ivy_vision/implicit.py
@@ -3,6 +3,37 @@ import ivy
 
 
 # noinspection PyUnresolvedReferences
+def sampled_volume_density_to_occupancy_probability(density, inter_sample_distance):
+    """
+    Compute probability of occupancy, given sampled volume densities and their associated inter-sample distances
+
+    :param density: The sampled density values *[batch_shape]*
+    :type density: array
+    :param inter_sample_distance: The inter-sample distances *[batch_shape]*
+    :type inter_sample_distance: array
+    :return: The occupancy probabilities *[batch_shape]*
+    """
+    return 1 - ivy.exp(-density*inter_sample_distance)
+
+
+def ray_termination_probabilities(density, inter_sample_distance):
+    """
+    Compute probability of occupancy, given sampled volume densities and their associated inter-sample distances
+
+    :param density: The sampled density values *[batch_shape,num_samples_per_ray]*
+    :type density: array
+    :param inter_sample_distance: The inter-sample distances *[batch_shape,num_samples_per_ray]*
+    :type inter_sample_distance: array
+    :return: The occupancy probabilities *[batch_shape]*
+    """
+
+    # BS x NSPR
+    occ_prob = sampled_volume_density_to_occupancy_probability(density, inter_sample_distance)
+    cumprod_one_m_occ_prob = ivy.cumprod(1 - occ_prob, -1)
+    return ivy.concatenate((occ_prob[..., 0:1], occ_prob[..., 1:] * cumprod_one_m_occ_prob[..., :-1]), -1)
+
+
+# noinspection PyUnresolvedReferences
 def stratified_sample(starts, ends, num_samples, batch_shape=None):
     """
     Perform stratified sampling, between start and end arrays. This operation divides the range into equidistant bins,
@@ -48,6 +79,55 @@ def stratified_sample(starts, ends, num_samples, batch_shape=None):
 
 
 # noinspection PyUnresolvedReferences
+def render_rays_via_termination_probabilities(radial_depths, features, densities, render_variance=False):
+    """
+    Render features onto the image plane, given rays sampled at radial depths with readings of
+    feature values and densities at these sample points.
+
+    :param radial_depths: Radial depth values *[batch_shape,num_samples_per_ray+1]*
+    :type radial_depths: array
+    :param features: Feature values at the sample points *[batch_shape,num_samples_per_ray,feat_dim]*
+    :type features: array
+    :param densities: Volume density values at the sample points *[batch_shape,num_samples_per_ray]*
+    :type densities: array
+    :param render_variance: Whether to also render the feature variance. Default is False.
+    :type render_variance: bool, optional
+    :return: The feature renderings along the rays, computed via the termination probabilities *[batch_shape,feat_dim]*
+    """
+
+    # BS x NSPR
+    d = radial_depths[..., 1:] - radial_depths[..., :-1]
+    ray_term_probs = ray_termination_probabilities(densities, d)
+    rendering = ivy.reduce_sum(ray_term_probs * features, -2)
+    if not render_variance:
+        return rendering
+    var = ivy.reduce_sum(ray_term_probs*(rendering - features)**2, -2)
+    return rendering, var
+
+
+# noinspection PyUnresolvedReferences
+def render_ray_variances_via_termination_probabilities(radial_depths, features, densities):
+    """
+    Render features onto the image plane, given rays sampled at radial depths with readings of
+    feature values and densities at these sample points.
+
+    :param radial_depths: Radial depth values *[batch_shape,num_samples_per_ray+1]*
+    :type radial_depths: array
+    :param features: Feature values at the sample points *[batch_shape,num_samples_per_ray,feat_dim]*
+    :type features: array
+    :param densities: Volume density values at the sample points *[batch_shape,num_samples_per_ray]*
+    :type densities: array
+    :return: The feature renderings along the rays, computed via the termination probabilities *[batch_shape,feat_dim]*
+    """
+
+    # BS x NSPR
+    rendering = render_rays_via_termination_probabilities(radial_depths, features, densities)
+    d = radial_depths[..., 1:] - radial_depths[..., :-1]
+    ray_term_probs = ray_termination_probabilities(densities, d)
+    return ivy.reduce_sum(ray_term_probs * features, -2)
+
+
+# noinspection PyUnresolvedReferences
 def render_rays_via_quadrature_rule(radial_depths, features, densities):
     """
     Render features onto the image plane, given rays sampled at radial depths with readings of
@@ -59,7 +139,7 @@ def render_rays_via_quadrature_rule(radial_depths, features, densities):
     :type features: array
     :param densities: Volume density values at the sample points *[batch_shape,num_samples_per_ray]*
     :type densities: array
-    :return: The feature renderins along the rays, computed via the quadrature rule *[batch_shape,feat_dim]*
+    :return: The feature renderings along the rays, computed via the quadrature rule *[batch_shape,feat_dim]*
     """
 
     # BS x NSPR
--- ivy_vision_tests/test_implicit.py
+++ ivy_vision_tests/test_implicit.py
@@ -1,6 +1,5 @@
 # global
 import numpy as np
-import ivy_tests.helpers as helpers
 
 # local
 from ivy_vision import implicit as ivy_imp
@@ -20,17 +19,43 @@ class ImplicitTestData(TestData):
         self.radial_depths = np.array([[0, 1, 2, 3],
                                        [1, 1.5, 2, 2.5],
                                        [2, 4, 5, 6]], np.float32)
+        self.inter_sample_distances = self.radial_depths[..., 1:] - self.radial_depths[..., :-1]
         self.features = np.array([[[0., 0.5, 0.7], [1., 0.2, 0.3], [0.1, 0.2, 0.3]],
                                   [[0.3, 0.2, 0.1], [0.5, 0.7, 0.3], [0.1, 0.9, 0.7]],
                                   [[0.6, 0.4, 0.2], [0.9, 0.3, 0.6], [0.4, 0.8, 0.5]]], np.float32)
         self.densities = np.array([[0.3, 2.4, 0.4], [0.2, 0.7, 0.4], [0.1, 0.3, 1.2]], np.float32)
 
-        self.quadrature_rendering = np.array([[0.06259394, 0.11119541, 0.1571928 ],
-                                              [0.12944466, 0.23419851, 0.13134202],
-                                              [0.2815921 , 0.20865306, 0.18783332]], np.float32)
+        self.occ_probs = np.array([[0.2591818, 0.9092821, 0.32967997],
+                                   [0.09516257, 0.29531187, 0.18126929],
+                                   [0.18126929, 0.2591818, 0.6988058]], np.float32)
+        self.ray_term_probs = np.array([[0.2591818, 0.6736127, 0.0221563],
+                                        [0.09516257, 0.26720923, 0.11558241],
+                                        [0.18126929, 0.21220009, 0.4238471]], np.float32)
+        # ToDo: verify the below renderings should indeed be unique, if not, find the bug.
+        self.quad_feature_rendering = np.array([[0.06259394, 0.11119541, 0.1571928],
+                                                [0.12944466, 0.23419851, 0.13134202],
+                                                [0.2815921, 0.20865306, 0.18783332]], np.float32)
+        self.term_prob_feature_rendering = np.array([[0.1132895, 0.43268824, 0.17733827],
+                                                     [0.14346276, 0.5127491, 0.33358333],
+                                                     [0.3136631, 0.51936793, 0.28570426]], np.float32)
+        self.term_prob_var_rendering = np.array([[0.08141835, 0.05083185, 0.00626953],
+                                                 [0.02940753, 0.0765848, 0.0730124],
+                                                 [0.11721418, 0.02952592, 0.02767936]], np.float32)
 
 
 td = ImplicitTestData()
+
+
+def test_sampled_volume_density_to_occupancy_probability(dev_str, call):
+    occ_prob = call(ivy_imp.sampled_volume_density_to_occupancy_probability, td.densities, td.inter_sample_distances)
+    assert occ_prob.shape == td.densities.shape
+    assert np.allclose(occ_prob, td.occ_probs)
+
+
+def test_ray_termination_probabilities(dev_str, call):
+    ray_term_probs = call(ivy_imp.ray_termination_probabilities, td.densities, td.inter_sample_distances)
+    assert ray_term_probs.shape == td.densities.shape
+    assert np.allclose(ray_term_probs, td.ray_term_probs)
 
 
 def test_stratified_sample(dev_str, call):
@@ -42,7 +67,16 @@ def test_stratified_sample(dev_str, call):
             assert res[i][j] < res[i][j+1]
 
 
+def test_render_rays_via_termination_probabilities(dev_str, call):
+    rendering, var = call(ivy_imp.render_rays_via_termination_probabilities, td.radial_depths, td.features,
+                          td.densities, render_variance=True)
+    assert rendering.shape == td.radial_depths.shape[:-1] + (3,)
+    assert var.shape == td.radial_depths.shape[:-1] + (3,)
+    assert np.allclose(rendering, td.term_prob_feature_rendering)
+    assert np.allclose(var, td.term_prob_var_rendering)
+
+
 def test_render_rays_via_quadrature_rule(dev_str, call):
     res = call(ivy_imp.render_rays_via_quadrature_rule, td.radial_depths, td.features, td.densities)
     assert res.shape == td.radial_depths.shape[:-1] + (3,)
-    assert np.allclose(res, td.quadrature_rendering)
+    assert np.allclose(res, td.quad_feature_rendering)
