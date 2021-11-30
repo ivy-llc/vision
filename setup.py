249d704cddd2363564157b65cde5c4d9357a7614
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-30 20:24:42 +0100

    modified setup.py so pypi long_description is derived from README.rst, but with all the raw html removed. Also changed so install_requires is inferred from requirements.txt.

--- setup.py
+++ setup.py
@@ -13,26 +13,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License..
 # ==============================================================================
-from distutils.core import setup
 import setuptools
+from pathlib import Path
+from distutils.core import setup
+
+
+def _strip(line):
+    return line.split(' ')[0].split('#')[0].split(',')[0]
+
+def is_html(line):
+    line_squashed = line.replace(' ', '')
+    if not line_squashed:
+        return False
+    if line_squashed[0] == '<' and line_squashed[-1] == '>':
+        return True
+    return False
+
+def is_raw_block(line):
+    line_squashed = line.replace(' ', '')
+    if len(line_squashed) < 11:
+        return False
+    if line_squashed[-11:] == '..raw::html':
+        return True
+    return False
+
+
+this_directory = Path(__file__).parent
+lines = (this_directory / "README.rst").read_text().split('\n')
+lines = [line for line in lines if not (is_html(line) or is_raw_block(line))]
+long_description = '\n'.join(lines)
 
 setup(name='ivy-vision',
       version='1.1.6',
       author='Ivy Team',
       author_email='ivydl.team@gmail.com',
       description='3D Vision functions with end-to-end support for deep learning developers, written in Ivy.',
-      long_description="""# What is Ivy Vision?\n\nIvy vision focuses predominantly on 3D vision, with functions for
-      camera geometry, image projections, co-ordinate frame transformations, forward warping, inverse warping, optical flow,
-      depth triangulation, voxel grids, point clouds, signed distance functions, and others.
-      Ivy currently supports Jax, TensorFlow, PyTorch, MXNet and Numpy. Check out the [docs](https://ivy-dl.org/vision) for more info!""",
-      long_description_content_type='text/markdown',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       url='https://ivy-dl.org/vision',
       project_urls={
             'Docs': 'https://ivy-dl.org/vision/',
             'Source': 'https://github.com/ivy-dl/vision',
       },
       packages=setuptools.find_packages(),
-      install_requires=['opencv-python', 'ivy-core', 'ivy-mech'],
+      install_requires=[_strip(line) for line in open('requirements.txt', 'r')],
       classifiers=['License :: OSI Approved :: Apache Software License'],
       license='Apache 2.0'
       )
