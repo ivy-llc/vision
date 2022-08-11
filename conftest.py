0f2a11bc2ca0cb65b7a741d994b1d3ed234e2e65
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-11 05:18:53 +0000

    update test config

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ from ivy_tests.test_ivy import helpers
 FW_STRS = ['numpy', 'jax', 'tensorflow', 'torch', 'mxnet']
 
 
-TEST_FRAMEWORKS: Dict[str, callable] = {'numpy': lambda: helpers.get_ivy_numpy(),
+TEST_BACKENDS: Dict[str, callable] = {'numpy': lambda: helpers.get_ivy_numpy(),
                                         'jax': lambda: helpers.get_ivy_jax(),
                                         'tensorflow': lambda: helpers.get_ivy_tensorflow(),
                                         'torch': lambda: helpers.get_ivy_torch(),
@@ -23,47 +23,31 @@ TEST_CALL_METHODS: Dict[str, callable] = {'numpy': helpers.np_call,
 
 
 @pytest.fixture(autouse=True)
-def run_around_tests(dev_str, f, wrapped_mode, compile_graph, call):
-    if wrapped_mode and call is helpers.tf_graph_call:
-        # ToDo: add support for wrapped_mode and tensorflow compilation
-        pytest.skip()
-    if wrapped_mode and call is helpers.jnp_call:
-        # ToDo: add support for wrapped_mode with jax, presumably some errenously wrapped jax methods
-        pytest.skip()
+def run_around_tests(dev_str, f, compile_graph, implicit, call, fw):
     if 'gpu' in dev_str and call is helpers.np_call:
         # Numpy does not support GPU
         pytest.skip()
-    ivy.clear_framework_stack()
+    ivy.clear_backend_stack()
     with f.use:
-        f.set_wrapped_mode(wrapped_mode)
-        ivy.set_default_device(dev_str)
-        yield
+        with ivy.DefaultDevice(dev_str):
+            yield
 
 
 def pytest_generate_tests(metafunc):
 
-    # dev_str
-    raw_value = metafunc.config.getoption('--dev_str')
+    # device
+    raw_value = metafunc.config.getoption('--device')
     if raw_value == 'all':
-        dev_strs = ['cpu', 'gpu:0', 'tpu:0']
+        devices = ['cpu', 'gpu:0', 'tpu:0']
     else:
-        dev_strs = raw_value.split(',')
+        devices = raw_value.split(',')
 
     # framework
-    raw_value = metafunc.config.getoption('--framework')
+    raw_value = metafunc.config.getoption('--backend')
     if raw_value == 'all':
-        f_strs = TEST_FRAMEWORKS.keys()
+        backend_strs = TEST_BACKENDS.keys()
     else:
-        f_strs = raw_value.split(',')
-
-    # wrapped_mode
-    raw_value = metafunc.config.getoption('--wrapped_mode')
-    if raw_value == 'both':
-        wrapped_modes = [True, False]
-    elif raw_value == 'true':
-        wrapped_modes = [True]
-    else:
-        wrapped_modes = [False]
+        backend_strs = raw_value.split(',')
 
     # compile_graph
     raw_value = metafunc.config.getoption('--compile_graph')
@@ -74,19 +58,26 @@ def pytest_generate_tests(metafunc):
     else:
         compile_modes = [False]
 
+    # with_implicit
+    raw_value = metafunc.config.getoption('--with_implicit')
+    if raw_value == "true":
+        implicit_modes = [True, False]
+    else:
+        implicit_modes = [False]
+
     # create test configs
     configs = list()
-    for f_str in f_strs:
-        for dev_str in dev_strs:
-            for wrapped_mode in wrapped_modes:
-                for compile_graph in compile_modes:
+    for backend_str in backend_strs:
+        for device in devices:
+            for compile_graph in compile_modes:
+                for implicit in implicit_modes:
                     configs.append(
-                        (dev_str, TEST_FRAMEWORKS[f_str](), wrapped_mode, compile_graph, TEST_CALL_METHODS[f_str]))
-    metafunc.parametrize('dev_str,f,wrapped_mode,compile_graph,call', configs)
+                        (device, TEST_BACKENDS[backend_str](), compile_graph, implicit, TEST_CALL_METHODS[backend_str], backend_str))
+    metafunc.parametrize('dev_str,f,compile_graph,implicit,call,fw', configs)
 
 
 def pytest_addoption(parser):
-    parser.addoption('--dev_str', action="store", default="cpu")
-    parser.addoption('--framework', action="store", default="numpy,jax,tensorflow,torch,mxnet")
-    parser.addoption('--wrapped_mode', action="store", default="false")
+    parser.addoption('--device', action="store", default="cpu")
+    parser.addoption('--backend', action="store", default="numpy,jax,tensorflow,torch")
     parser.addoption('--compile_graph', action="store", default="true")
+    parser.addoption('--with_implicit', action="store", default="false")
